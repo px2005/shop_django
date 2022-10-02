8abf6d991ecb37cc451204d5b1842c91e6b56708
Александр Горыничев <px2005@yandex.ru>
2022-10-02 13:00:33 +0300

    create order v.2.0.1

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^cart/', include('cart.urls')),
+    re_path(r'^orders/', include('orders.urls')),
     re_path(r'^', include('shop.urls'))
 ]
 
