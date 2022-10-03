20646349fdc59853122a01cb7a997e9fb66a8a3e
Александр Горыничев <px2005@yandex.ru>
2022-10-03 14:00:40 +0300

    create cart form widget

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+# PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    # quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.IntegerField(
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control text-center',
+            'type': 'number',
+            'value': '1',
+        })
+    )
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
